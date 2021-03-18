1f123f9a64ef1e2d4cc92b431f92bec6ed211fd1
Monish-Samuel <samuelmonish1@gmail.com>
2021-03-18 11:15:43 +0530

    added new comments

--- output.py
+++ output.py
@@ -1,7 +1,10 @@
 import Alphabet
 
 
+# Method to print Output
 def run(sentence, character):
+    """this method imports the methods from the Alphabet file and prints the output for the input given in this
+    method"""
     print(Alphabet.word_length(sentence))
     print(Alphabet.matching_char(sentence, character))
     print(Alphabet.palindrome_or_not(sentence))
